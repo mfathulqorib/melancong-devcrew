40b62d8b81c7d95cd98c7fa312625c4c512b3938
calmbing <mfathulqorib97@gmail.com>
2023-12-21 20:44:49 +0700

    chores: delete unused line of code

--- src/app/api/v1/upload/route.js
+++ src/app/api/v1/upload/route.js
@@ -14,8 +14,6 @@ export async function POST(req) {
             Dir: directory
         })
     } catch (error) {
-        console.log(process.env.AWS_ACCESS_KEY_ID)
-        console.log(process.env.AWS_SECRET_ACCESS_KEY)
         console.log(error)
     }
 
