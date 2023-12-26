b05f6da1f8fc4ece9e03b40e476e5c4461a2f3e3
4212ganteng <azizmuslim051@gmail.com>
2023-12-26 08:51:41 +0700

    feat:user confirmation - user confirm email with token

--- src/app/api/v1/auth/confirmation/route.js
+++ src/app/api/v1/auth/confirmation/route.js
@@ -11,14 +11,44 @@ export async function PATCH(req) {
     console.log({ user });
     const gettoken = await prisma.tokenEmail.findMany();
 
-    const findUser = await prisma.tokenEmail.findUnique({
+    const findUserToken = await prisma.tokenEmail.findUnique({
       where: {
         token,
         userId: user,
       },
     });
 
-    return res.json({ data: findUser, alltoken: gettoken });
+    if (!findUserToken) {
+      return res.json({ error: "Your verification link may have expired." }, { status: 400 });
+    }
+
+    const findUser = await prisma.user.findUnique({
+      where: {
+        id: user,
+      },
+    });
+
+    if (!findUser) {
+      return res.json(
+        { error: "We were unable to find a user for this verification. Please SignUp!" },
+        { status: 400 }
+      );
+    }
+
+    if (findUser.isVerified) {
+      return res.json({ error: "User has been already verified. Please Login" }, { status: 200 });
+    }
+
+    const updated = await prisma.user.update({
+      where: {
+        id: user,
+      },
+      data: {
+        isVerified: true,
+      },
+    });
+
+    return res.json({ message: "Your account has been successfully verified" }, { status: 200 });
   } catch (error) {
     console.log(error);
     return res.json({ error: "Something went wrong. Please try again later", error }, { status: 500 });
