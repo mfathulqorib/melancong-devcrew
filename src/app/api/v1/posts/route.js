e7c90eb0e0e131bd66ac273ca4cdfe008663a541
4212ganteng <azizmuslim051@gmail.com>
2023-12-27 09:11:43 +0700

    feat:create post'

--- src/app/api/v1/posts/route.js
+++ src/app/api/v1/posts/route.js
@@ -1,3 +1,5 @@
+import { uploadFile } from "@/lib/uploadFile";
+import prisma from "@/utils/prisma";
 import { verify } from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse as res } from "next/server";
@@ -13,9 +15,8 @@ export async function POST(req) {
   const longitude = formData.get("longitude");
   const address = formData.get("address");
   const city = formData.get("city");
-  const categories = formData.get("categories");
+  const categories = formData.getAll("categories");
   const images = formData.getAll("images");
-  console.log("images", images);
 
   //   get detail user
   const cookieStorage = cookies();
@@ -23,28 +24,75 @@ export async function POST(req) {
   const decoded = verify(token, process.env.JWT_SECRET);
   const userId = decoded.id;
 
+  console.log("categories", categories);
+
+  let postId = "";
   try {
-    // const createPost = await prisma.post.create({
-    //   data: {
-    //     title,
-    //     desc,
-    //     budget: Number(budget) || 0,
-    //     slug: slugify(slug, { lower: true, replacement: "-" }),
-    //     officeHours,
-    //     latitude,
-    //     longitude,
-    //     address,
-    //     city,
-    //   },
-    // });
-    // make relation post_category
-    // categories.forEach(async (category) => {
-    //   const postCategory = await prisma.postCategory.create({
-    //     postId: createPost.id,
-    //     categoryId: category,
-    //   });
-    // });
-    // make relation post_image
+    try {
+      const createPost = await prisma.post.create({
+        data: {
+          title,
+          desc,
+          budget: Number(budget) || 0,
+          slug: slugify(title, { lower: true, replacement: "-" }),
+          officeHours,
+          latitude,
+          longitude,
+          address,
+          city,
+          userId,
+        },
+      });
+
+      if (!createPost) {
+        return res.json({ error: "failed create post" }, { status: 500 });
+      }
+
+      (postId = createPost.id),
+        // make relation post_category
+        categories.forEach(async (category) => {
+          const checkCategory = await prisma.findUnique({
+            where: {
+              id: category,
+            },
+          });
+          if (!checkCategory) {
+            return res.json({ error: `${checkCategory}, category not found` }, { status: 404 });
+          }
+
+          const postCategory = await prisma.postCategory.create({
+            data: { postId, categoryId: category },
+          });
+        });
+
+      // make relation post_image
+      images.forEach(async (element) => {
+        const imageSave = await prisma.postImage.create({
+          data: {
+            postId,
+            name: element.name,
+          },
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      return res.json({ error: `Something went wrong. Please try again later, ${error}` }, { status: 500 });
+    }
+
+    // save to s3
+    try {
+      images.forEach(async (image) => {
+        const UploadPostIMages = await uploadFile({
+          Body: image,
+          Dir: `posts/${postId}`,
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      return res.json({ error: `Something went wrong. Please try again later, ${error}` }, { status: 500 });
+    }
+
+    return res.json({ message: "success create post " }, { status: 200 });
   } catch (error) {
     console.log(error);
     return res.json({ error: `Something went wrong. Please try again later, ${error}` }, { status: 500 });
