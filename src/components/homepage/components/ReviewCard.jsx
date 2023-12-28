3f1b2a88795ab53e199e3a92f8fba75a142f5cee
calmbing <mfathulqorib97@gmail.com>
2023-12-28 11:54:00 +0700

    update

--- src/components/homepage/components/ReviewCard.jsx
+++ src/components/homepage/components/ReviewCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Image from "next/image";
 
-export const ReviewCard = ({ img }) => {
+export const ReviewCard = ({ img, name }) => {
   return (
-    <div className="h-full min-w-[200px] rounded-lg bg-white shadow-md shadow-black/30 sm:w-[300px]">
+    <div className="h-full min-w-[200px] rounded-lg bg-white shadow-md shadow-black/30 sm:min-w-[300px]">
       <div className="relative h-[45%] overflow-hidden rounded-t-lg border-b-1 border-black sm:h-[50%]">
         <Image
           width={0}
@@ -18,7 +18,7 @@ export const ReviewCard = ({ img }) => {
           priority
         />
       </div>
-      <div>Review content 1</div>
+      <div>{name}</div>
     </div>
   );
 };
--- src/components/homepage/components/TrendingDestination.jsx
+++ src/components/homepage/components/TrendingDestination.jsx
@@ -1,25 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useContext } from "react";
 import { ReviewCard } from "./ReviewCard";
 import { Header3 } from "./Header3";
-
-const fakeRes = [
-  {
-    img: "/fake_image.avif",
-    desc: "Gunung Bromo 1",
-  },
-  {
-    img: "/fake_image.avif",
-    desc: "Gunung Bromo 2",
-  },
-];
+import { AppContext } from "@/components/providers";
 
 export const TrendingDestination = () => {
+  const { fakeRes } = useContext(AppContext);
+
   return (
     <main className="my-2 px-[5vw] py-2 sm:my-4 sm:px-[10vw] ">
       <Header3 text={"🔥 Destinasi yang lagi tren"} />
       <section className="mt-2 flex h-[290px] w-full gap-3 overflow-x-auto px-1 py-2 sm:h-[370px] sm:gap-5">
-        {fakeRes.map(({ img }, index) => {
-          return <ReviewCard img={img} key={index} />;
+        {fakeRes.map(({ img, name }, index) => {
+          return <ReviewCard img={img} name={name} key={index} />;
         })}
       </section>
     </main>
