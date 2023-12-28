9bb92bca24ab6e273ebb6d1ccbf3a7e8887bd689
calmbing <mfathulqorib97@gmail.com>
2023-12-28 08:15:25 +0700

    feat: try mockup res forr map in homepage

--- src/components/homepage/components/Reviews.jsx
+++ src/components/homepage/components/Reviews.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import { ReviewCard } from "./ReviewCard";
 
-const fakeRes = ["/fake_image.avif", "/fake_image.avif"];
-const fakeResObj = [
+const fakeRes = [
   {
-    img: ["/fake_image.avif", "/fake_image.avif"],
-    desc: ["Gunung Bromo 1", "Gunung Bromo 2"],
+    img: "/fake_image.avif",
+    desc: "Gunung Bromo 1",
+  },
+  {
+    img: "/fake_image.avif",
+    desc: "Gunung Bromo 2",
   },
 ];
-const { img } = fakeResObj[0];
-console.log(img);
-fakeResObj.map(({ img }) => {
-  img.map((items) => {
-    console.log(items);
-  });
-});
 
 export const Reviews = () => {
   return (
@@ -23,7 +19,8 @@ export const Reviews = () => {
         🔥Trending review dari sobat jalan-jalan
       </h2>
       <section className="mt-2 flex h-[290px] w-full gap-3 overflow-x-auto px-1 py-2 sm:h-[370px] sm:gap-5">
-        {fakeRes.map((img, index) => {
+        {fakeRes.map(({ img }, index) => {
+          console.log(img);
           return <ReviewCard img={img} key={index} />;
         })}
       </section>
