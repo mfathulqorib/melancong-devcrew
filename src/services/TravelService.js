262201fd0a5cd5ba8db8084362eeb862d75f8326
calmbing <mfathulqorib97@gmail.com>
2023-12-30 03:26:31 +0700

    update base url travel service to test CORS

--- src/services/TravelService.js
+++ src/services/TravelService.js
@@ -9,7 +9,7 @@ const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImExNjYyODRhLTNlNjUtNGI0MS05ZWIzLWFmMDJlMjIyMDVkMyIsInVzZXJuYW1lIjoibWFya28iLCJuYW1lIjoibWFya28gZWZlbmRpIiwiYmlvIjoic29sbyB0cmF2ZWxsZXIiLCJlbWFpbCI6ImRva3RlcmZhcm1hMDAxQGdtYWlsLmNvbSIsImF2YXRhciI6IiIsInJvbGVJZCI6ImRldmNyM3ctNDUtdTUzciIsImlhdCI6MTcwMzgzOTUwMCwiZXhwIjoxNzA0NDQ0MzAwfQ.jdoOLTQZvPsQ_aW0qL7hpWj5eVKbzhPgPePc-_BAfqc";
 
 export const travelService = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://melancong-devcrew.vercel.app/api/v1",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
