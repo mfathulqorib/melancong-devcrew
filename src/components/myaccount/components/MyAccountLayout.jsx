a1386e40d65d53ba5c5d838cf7951dd543e6860c
calmbing <mfathulqorib97@gmail.com>
2023-12-30 23:54:20 +0700

    fix: fix payload in /myaccount page

--- src/components/myaccount/components/MyAccountLayout.jsx
+++ src/components/myaccount/components/MyAccountLayout.jsx
@@ -1,11 +1,22 @@
 import { AccountNavbar } from "@/components/AccountNavbar";
 import { Footer } from "@/components/Footer";
+import { SECRET_KEY, TOKEN } from "@/utils/ApiUrl";
+import { jwtExtract } from "@/utils/jwtExtract";
+import { capitalizeEachWord, slugForUiApi } from "@/utils/sentenceTraversal";
 import React from "react";
 
 export const MyAccountLayout = ({ children }) => {
+  const payload = jwtExtract(TOKEN, SECRET_KEY);
+  const { name, username, email } = payload;
+
   return (
     <>
-      <AccountNavbar />
+      <AccountNavbar
+        name={capitalizeEachWord(name)}
+        username={capitalizeEachWord(username)}
+        slug={slugForUiApi(name)}
+        email={email}
+      />
       <main>{children}</main>
       <Footer />
     </>
