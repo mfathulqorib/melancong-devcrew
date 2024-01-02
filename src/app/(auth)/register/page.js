6447450aa6c6c95ad1783b798188381dcdb962ad
calmbing <mfathulqorib97@gmail.com>
2024-01-02 14:10:27 +0700

    feat: add hyperlink in login and register page

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,16 +1,19 @@
-import { Button, ButtonGroup, Input } from "@nextui-org/react"
-import { Logo } from "@/components/Logo"
+import { Button, ButtonGroup, Input } from "@nextui-org/react";
+import { Logo } from "@/components/Logo";
+import Link from "next/link";
 
 export default function App() {
-  const placements = ["outside"]
+  const placements = ["outside"];
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <Logo className="flex flex-col items-center justify-center"></Logo>
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-2 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white mb-10">
+      <div className="mx-auto flex flex-col items-center justify-center px-6 py-8 md:h-screen lg:py-0">
+        <Link href="/">
+          <Logo className="flex flex-col items-center justify-center" />
+        </Link>
+        <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-2 xl:p-0 dark:border dark:border-gray-700 dark:bg-gray-800">
+          <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
+            <h1 className="mb-10 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Buat Akun
             </h1>
             <div className="my-6">
@@ -57,22 +60,21 @@ export default function App() {
             </div>
             <Button
               color="primary"
-              className="w-full text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 mb-4"
+              className="mb-4 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
               Buat Akun
             </Button>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
               Sudah Punya Akun?{" "}
-              <a
-                href="/login"
-                className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-              >
-                Yuk Masuk
-              </a>
+              <Link href="/login">
+                <span className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                  Yuk Masuk
+                </span>
+              </Link>
             </p>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
