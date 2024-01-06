9a341e8f4a33fe0b0002db9ed6dab319700f4882
calmbing <mfathulqorib97@gmail.com>
2024-01-06 18:32:00 +0700

    feat: preview reviews user in dashboard user

--- src/app/dashboard/user/page.js
+++ src/app/dashboard/user/page.js
@@ -1,16 +1,34 @@
 import { PerformaReview } from "@/components/myaccount/components/PerformaReview";
 import { Reviews } from "@/components/myaccount/components/Reviews";
+import { travelService } from "@/services/TravelService";
+import { SECRET_KEY } from "@/utils/ApiUrl";
+import { jwtExtract } from "@/utils/jwtExtract";
+import { cookies } from "next/headers";
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token").value;
+  const payload = jwtExtract(token, SECRET_KEY);
+  const { id } = payload;
+  const getReviews = async () => {
+    try {
+      const res = await travelService.get(`/comment?userId=${id}`);
+      if (res.status === 200) {
+        return res.data.data;
+      }
+    } catch (error) {}
+  };
+  const myReviews = await getReviews();
+
   return (
     <main className="min-h-[calc(100vh-50px)] px-[5vw] pt-[50px] sm:min-h-[calc(100vh-60px)] sm:px-[10vw] sm:pt-[60px]">
       <section className="mt-3 sm:mt-5">
         <h2 className="text-xl font-bold sm:text-2xl">Review Kamu</h2>
-        <PerformaReview />
+        <PerformaReview totalReviews={myReviews?.length || 0} />
       </section>
       <section className="mt-3 sm:mt-5">
-        <Reviews />
+        <Reviews myReviews={myReviews} />
       </section>
     </main>
   );
--- src/components/myaccount/components/Reviews.jsx
+++ src/components/myaccount/components/Reviews.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { NoReview } from "@/components/myaccount/components/icon/NoReview";
+import { travelService } from "@/services/TravelService";
+import { set } from "date-fns";
+import { CommentCard } from "@/components/destination/components/CommentCard";
 
-export const Reviews = () => {
-  return (
-    <>
-      <h3 className="text-lg font-semibold sm:text-xl">Kumpulan Review-mu</h3>
-      <div>
+export const Reviews = ({ myReviews }) => {
+  const EmptyState = () => {
+    return (
+      <>
         <div className="m-0 mx-auto w-fit">
           <NoReview />
         </div>
@@ -18,7 +21,42 @@ export const Reviews = () => {
             kunjungi, ya!
           </p>
         </div>
+      </>
+    );
+  };
+
+  const renderReviews = () => {
+    return (
+      <div className="flex w-full justify-center">
+        <div className="flex h-full w-fit flex-wrap items-center gap-4 py-2">
+          {/* Comment Card */}
+          {myReviews.map(({ id, post, message, createdAt }) => {
+            return (
+              <CommentCard
+                name={post.title}
+                message={message}
+                createdAt={createdAt}
+                key={id}
+              />
+            );
+          })}
+        </div>
       </div>
+    );
+  };
+
+  // useEffect(() => {
+  //   getReviews();
+  // }, []);
+
+  useEffect(() => {
+    console.log(myReviews);
+  }, [myReviews]);
+
+  return (
+    <>
+      <h3 className="text-lg font-semibold sm:text-xl">Kumpulan Review-mu</h3>
+      {myReviews.length ? renderReviews() : <EmptyState />}
     </>
   );
 };
